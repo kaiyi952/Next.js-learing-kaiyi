f94a5c180b6daa0064717f05896335c69bfd58a7
Kaiyi952 <irishe952@gmail.com>
2024-06-25 14:29:29 -0400

    6.24

---
+++ app/info/contact/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function ContactPage() {
+  return (
+    <div>
+      <h1 className='text-7xl'>Contact Page</h1>
+      <Link href="/about" className="text-xl text-blue-500 inline-block mt-8">back home</Link>
+      </div>
+  )
+}
+
+export default ContactPage;
